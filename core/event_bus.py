4786a1ae191977fa92dbdb6b86402e998d7a6455
Luis <lraulam.03@gmail.com>
2025-11-11 10:38:47 -0600

    Migrated to event driven

---
+++ core/event_bus.py
@@ -0,0 +1,14 @@
+# core/event_bus.py
+class EventBus:
+    def __init__(self):
+        self.subscribers = {}
+
+    def subscribe(self, event_name, handler):
+        if event_name not in self.subscribers:
+            self.subscribers[event_name] = []
+        self.subscribers[event_name].append(handler)
+
+    def publish(self, event_name, data=None):
+        if event_name in self.subscribers:
+            for handler in self.subscribers[event_name]:
+                handler(data)
